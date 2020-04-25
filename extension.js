3ec762ac9ab99718e7a884b0bba55fd19c85cba4
lzbz <buchholz.lorenz@gmail.com>
2020-04-25 16:57:30 +0200

    remove lock, suspend, seperator2 + move settings

--- extension.js
+++ extension.js
@@ -6,15 +6,12 @@ const PanelMenu = imports.ui.panelMenu;
 const PopupMenu = imports.ui.popupMenu;
 const SystemActions = imports.misc.systemActions;
 
-let logout;
-let switchUser;
-let separator1;
-let lock;
-let suspend;
-let power;
-let separator2;
 let orientationLock;
 let settings;
+let separator1;
+let logout;
+let switchUser;
+let power;
 
 var _bringOut = new Lang.Class({
     Name: "Bring Out Submenu Of Power Off/Logout Button and Rearrange the Order of System Menu.",
@@ -30,37 +27,6 @@ _init: function() {
 _createSubMenu: function() {
 
 let bindFlags = GObject.BindingFlags.DEFAULT | GObject.BindingFlags.SYNC_CREATE;
-
-logout = new PopupMenu.PopupImageMenuItem(_('Logout'), 'system-logout-symbolic');
-logout.connect('activate', () => { this._systemActions.activateLogout(); });
-Menu.addMenuItem(logout);
-this._systemActions.bind_property('can-logout', logout, 'visible', bindFlags);
-
-switchUser = new PopupMenu.PopupImageMenuItem(_('Switch User'), 'system-switch-user-symbolic.svg');
-switchUser.connect('activate', () => { this._systemActions.activateSwitchUser(); });
-Menu.addMenuItem(switchUser);
-this._systemActions.bind_property('can-switch-user', switchUser, 'visible', bindFlags);
-        
-separator1 = new PopupMenu.PopupSeparatorMenuItem;
-Menu.addMenuItem(separator1);
-
-lock = new PopupMenu.PopupImageMenuItem(_('Lock'), 'changes-prevent-symbolic');
-lock.connect('activate', () => { this._systemActions.activateLockScreen(); });
-Menu.addMenuItem(lock);
-this._systemActions.bind_property('can-lock-screen', lock, 'visible', bindFlags);
-
-suspend = new PopupMenu.PopupImageMenuItem(_('Suspend'), 'media-playback-pause-symbolic');
-suspend.connect('activate', () => { this._systemActions.activateSuspend(); });
-Menu.addMenuItem(suspend);
-this._systemActions.bind_property('can-suspend', suspend, 'visible', bindFlags);
-        
-power = new PopupMenu.PopupImageMenuItem(_('Power Off'), 'system-shutdown-symbolic');
-power.connect('activate', () => { this._systemActions.activatePowerOff(); });
-Menu.addMenuItem(power);
-this._systemActions.bind_property('can-power-off', power, 'visible', bindFlags);
-
-separator2 = new PopupMenu.PopupSeparatorMenuItem;
-Menu.addMenuItem(separator2);
 
 orientationLock = new PopupMenu.PopupImageMenuItem( this._systemActions.getName('lock-orientation'), this._systemActions.orientation_lock_icon);
 orientationLock.connect('activate', () => { this._systemActions.activateLockOrientation(); });
@@ -78,18 +44,34 @@ if (settingsApp) {
 settings.connect('activate', () => { Main.overview.hide(); settingsApp.activate(); });
 Menu.addMenuItem(settings); }
 else { log('Missing required core component Settings, expect trouble…'); settings = new St.Widget(); }
+
+separator1 = new PopupMenu.PopupSeparatorMenuItem;
+Menu.addMenuItem(separator1);
+
+logout = new PopupMenu.PopupImageMenuItem(_('Logout'), 'system-log-out-symbolic');
+logout.connect('activate', () => { this._systemActions.activateLogout(); });
+Menu.addMenuItem(logout);
+this._systemActions.bind_property('can-logout', logout, 'visible', bindFlags);
+
+switchUser = new PopupMenu.PopupImageMenuItem(_('Switch User'), 'system-switch-user-symbolic.svg');
+switchUser.connect('activate', () => { this._systemActions.activateSwitchUser(); });
+Menu.addMenuItem(switchUser);
+this._systemActions.bind_property('can-switch-user', switchUser, 'visible', bindFlags);
+
+power = new PopupMenu.PopupImageMenuItem(_('Power Off'), 'system-shutdown-symbolic');
+power.connect('activate', () => { this._systemActions.activatePowerOff(); });
+Menu.addMenuItem(power);
+this._systemActions.bind_property('can-power-off', power, 'visible', bindFlags);
+
 },
     
 destroy: function () {
-Menu.box.remove_actor(logout);
-Menu.box.remove_actor(switchUser);
-Menu.box.remove_actor(separator1);
-Menu.box.remove_actor(lock);
-Menu.box.remove_actor(suspend);
-Menu.box.remove_actor(power);
-Menu.box.remove_actor(separator2);
 Menu.box.remove_actor(orientationLock);
 Menu.box.remove_actor(settings);
+Menu.box.remove_actor(separator1);
+Menu.box.remove_actor(logout);
+Menu.box.remove_actor(switchUser);
+Menu.box.remove_actor(power);
 Menu.box.add_actor(this._system.menu.actor)
 }
 });
